ef407291fad0fea71728efbb754b3c6c42a8a028
gurongjing@163.com <gurongjing@163.com>
2017-01-17 15:24:55 +0800

    add some location.herf

--- views/js/index.js
+++ views/js/index.js
@@ -2,13 +2,16 @@
  * Created by Gurongjing on 2017/1/17.
  */
 
-function first(name){
-    alert(name);
-}
-
 function le_ajax(){
     var xhr = new XMLHttpRequest();
     xhr.open("get","https://registry.npm.taobao.org/",true);
     xhr.send(null);
     console.log(xhr.responseText);
+}
+
+function le_tang(){
+    var tli = document.getElementById("tangshi");
+    tli.onclick=function(){
+        window.location.href="a.html";
+    }
 }
